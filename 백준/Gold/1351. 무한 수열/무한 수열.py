79c07835ae6da316ce5e7b1c2fcf1619030949ef
minsangOh <138945498+minsangOh@users.noreply.github.com>
2023-10-14 17:37:46 +0900

    [Gold V] Title: 무한 수열, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -1,8 +1,22 @@
+import sys
 from collections import defaultdict
+
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 def dfs(n):
     if n == 0: return 1
     if seq[n]: return seq[n]
-    seq[n] = dfs(n // p) + dfs( n // q); return seq[n]
 
-n, p, q = map(int, input().split()); seq = defaultdict(int); print(dfs(n))
+    div1 = n // p
+    div2 = n // q
+
+    seq[n] = dfs(div1) + dfs(div2)
+    return seq[n]
+
+
+n, p, q = map(int, input().split())
+seq = defaultdict(int)
+
+print(dfs(n))
