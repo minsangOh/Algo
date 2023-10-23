2366feab7c715df920416ec146a47ee4f311fe8c
minsangOh <138945498+minsangOh@users.noreply.github.com>
2023-10-23 17:31:06 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 120 ms, Memory: 117056 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -1,3 +1,16 @@
+"""
+23 / 10 / 20 알고 스터디
+피보나치 수의 확장
+"""
+
+"""
+-7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7  8  9  10
+13 -8  5 -3  2 -1  1 0 1 1 2 3 5 8 13 21 34 55
+"""
+
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
 n = int(input())
 nn = abs(n)
 
@@ -14,5 +27,4 @@ elif nn % 2 == 1:
     print(1)
 else:
     print(-1)
-
 print(dp[nn])
