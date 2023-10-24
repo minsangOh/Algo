513af15bc827d9b88312f91bf4676eba1987092f
minsangOh <138945498+minsangOh@users.noreply.github.com>
2023-10-24 19:54:48 +0900

    [Gold III] Title: 학교 탐방하기, Time: 668 ms, Memory: 197492 KB -BaekjoonHub

---
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.py
@@ -0,0 +1,42 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+
+def find(x):
+    if visited[x] == x:
+        return x
+    visited[x] = find(visited[x])
+    return visited[x]
+
+
+def union(a, b, c):
+    fa, fb = find(a), find(b)
+    if fa == fb:
+        return 0
+
+    if fa > fb:
+        visited[fb] = fa
+    else:
+        visited[fa] = fb
+    if c == 1:
+        return 0
+    else:
+        return 1
+
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(m+1)]
+
+arr.sort(key= lambda x:x[2])
+visited = [i for i in range(n + 1)]
+Max = 0
+for a,b,c in arr:
+    Max += union(a, b, c)
+
+arr.sort(key= lambda x:-x[2])
+visited = [i for i in range(n + 1)]
+Min = 0
+for a,b,c in arr:
+    Min += union(a, b, c)
+
+print(Max ** 2 - Min ** 2)
