3b1336183993f825faf25b7b52edd36717fcb3e7
minsangOh <138945498+minsangOh@users.noreply.github.com>
2023-10-24 11:53:13 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 456 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,12 +1,10 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
-
-
+dp = [0]*1000001
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 1000001):
+    dp[i] = (dp[i-1]+dp[i-2]+dp[i-3]) % 1000000009
 T = int(input())
 for TC in range(1,T+1):
     n = int(input())
-    dp = [0]*1000001
-    dp[1], dp[2], dp[3] = 1, 2, 4
-    for i in range(4, n+1):
-        dp[i] = (dp[i-1]+dp[i-2]+dp[i-3]) % 1000000009
     print(dp[n])
