5281fd65779c10d38694ccf66a32e2847d430532
minsangOh <138945498+minsangOh@users.noreply.github.com>
2024-03-23 20:22:21 +0900

    [Silver V] Title: 집합, Time: 1132 ms, Memory: 127892 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,30 +1,21 @@
 import sys
 def input(): return sys.stdin.readline()
 
-
 m = int(input())
-S = set()
+S = 0
 
 for _ in range(m):
     lst = input().split()
     if len(lst) == 1:
-        if lst[0] == 'all':
-            S = set([i for i in range(1, 21)])
-        else:
-            S = set()
+        if lst[0] == 'all': S = (1 << 21) - 1
+        else: S = 0
     else:
         st, x = lst[0], int(lst[1])
-        if st == "add":
-            S.add(x)
-        elif st == "remove":
-            S.discard(x)
-        elif st == "check":
-            if x in S:
+        if st == 'add': S |= (1 << x)
+        elif st == 'remove': S &= ~(1 << x)
+        elif st == 'check':
+            if S & (1 << x):
                 print(1)
             else:
                 print(0)
-        elif st == "toggle":
-            if x in S:
-                S.discard(x)
-            else:
-                S.add(x)
+        elif st == 'toggle':S ^= (1 << x)
