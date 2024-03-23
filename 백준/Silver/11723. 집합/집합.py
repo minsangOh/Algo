6c0bec7a418ffe4569747951097a6e52d2d4fe0f
minsangOh <138945498+minsangOh@users.noreply.github.com>
2024-03-23 20:23:29 +0900

    [Silver V] Title: 집합, Time: 3804 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,5 +1,8 @@
 import sys
+
+
 def input(): return sys.stdin.readline()
+
 
 m = int(input())
 S = 0
@@ -7,15 +10,20 @@ S = 0
 for _ in range(m):
     lst = input().split()
     if len(lst) == 1:
-        if lst[0] == 'all': S = (1 << 21) - 1
-        else: S = 0
+        if lst[0] == 'all':
+            S = (1 << 21) - 1  # 21의 비트 생성 후 1로 초기화
+        else:
+            S = 0
     else:
         st, x = lst[0], int(lst[1])
-        if st == 'add': S |= (1 << x)
-        elif st == 'remove': S &= ~(1 << x)
+        if st == 'add':
+            S |= (1 << x)  # x 비트에 1 설정(or 연산자)
+        elif st == 'remove':
+            S &= ~(1 << x)  # x 비트에 0 설정 (and, not 연산자)
         elif st == 'check':
-            if S & (1 << x):
+            if S & (1 << x):  # S의 x번째에 있으면 1 출력
                 print(1)
             else:
                 print(0)
-        elif st == 'toggle':S ^= (1 << x)
+        elif st == 'toggle':
+            S ^= (1 << x)  # x 비트 반전 (Xor 연산자)
