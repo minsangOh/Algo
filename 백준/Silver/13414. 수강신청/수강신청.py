aefbb596585876891b4daf8f6885d8eb41d1efbb
minsangOh <138945498+minsangOh@users.noreply.github.com>
2024-04-09 11:44:48 +0900

    [Silver III] Title: 수강신청, Time: 448 ms, Memory: 212944 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -1,22 +1,17 @@
 import sys
-from collections import deque, defaultdict
 
 
 def input(): return sys.stdin.readline()
 
 
 student, length = map(int, input().split())
-lst = [(input())for _ in range(length)]
-
-dic = defaultdict()
+lst = [input() for _ in range(length)]
+dic = {}
 
 for number in lst:
     if number in dic:
         dic.pop(number)
-        dic[number] = 1
-    else:
-        dic[number] = 1
-
+    dic[number] = 1
 
 for idx, key in enumerate(dic.keys()):
     if idx == student: break
