5c204d4711f359d0f4b441e1b29d8324fb33f69b
minsangOh <138945498+minsangOh@users.noreply.github.com>
2024-05-23 09:35:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 180 ms, Memory: 112504 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,51 @@
+n, m, start = map(int, input().split())
+lst = [[] for _ in range(n + 1)]
+used = [0] * (n + 1)
+for i in range(m):
+    s, e = map(int, input().split())
+    lst[s].append(e)
+    lst[e].append(s)
+
+# 리스트 내부의 리스트 정렬
+for i in range(1, n + 1):
+    lst[i].sort()
+
+
+# DFS 탐색
+def dfs(now):
+    print(now, end=' ')
+    for i in lst[now]:
+        if used[i] == 1: continue
+        used[i] = 1
+        dfs(i)
+
+
+used[start] = 1
+dfs(start)
+
+# BFS 탐색
+from collections import deque
+
+used = [0] * (n + 1)
+ans = []
+
+
+def bfs(now):
+    q = deque()
+    q.append(now)
+
+    while q:
+        n = q.popleft()
+        ans.append(n)
+
+        for i in lst[n]:
+            if used[i] == 1:
+                continue
+            used[i] = 1
+            q.append(i)
+
+
+used[start] = 1
+bfs(start)
+print()
+print(*ans)
