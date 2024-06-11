b9019f69da2a428de51e88e5e5a6a5d064bd9654
minsangOh <138945498+minsangOh@users.noreply.github.com>
2024-06-11 20:01:38 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -5,9 +5,7 @@ using namespace std;
 int main() {
     int a, b, c;
     while (cin >> a >> b >> c) {
-        int case1 = (c - b) - 1;
-        int case2 = (b - a) - 1;
-        cout << max(case1, case2) << endl;
+        cout << max(c - b, b - a) - 1 << endl;
     }
     return 0;
 }
